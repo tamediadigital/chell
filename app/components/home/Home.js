07815f862c3d659504b9ec5e2c7a54b083d1b216
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-01-30 02:29:47 +0100

    Save to local storage recently visited rooms

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -16,6 +16,21 @@ export default class Home extends Component<Props, DefaultProps, State> {
   }
 
   handleSubmit(event) {
+    const lastVisited = {
+      time: new Date(),
+      room: this.state.roomName
+    };
+    let recentlyVisited = localStorage.getItem('recentlyVisited');
+    if (recentlyVisited) {
+      const items = JSON.parse(recentlyVisited);
+      items.items.push(lastVisited);
+      localStorage.setItem('recentlyVisited', JSON.stringify(items));
+    } else {
+      recentlyVisited = { items: [lastVisited] };
+      localStorage.setItem('recentlyVisited', JSON.stringify(recentlyVisited));
+    }
+
+    localStorage.setItem('lastVisited', JSON.stringify(lastVisited));
     console.log('A room name was submitted: ' + this.state.roomName);
     event.preventDefault();
   }
