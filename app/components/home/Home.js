ea7951f8a1eb4fd14ba3c81dc705ca672f813d31
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-01-30 01:17:59 +0100

    Submit room name

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -2,8 +2,28 @@
 import React, { Component } from 'react';
 import styles from './Home.css';
 
+type Props = { roomName: string };
+type DefaultProps = { roomName: string };
+type State = { roomName: string };
 
-export default class Home extends Component {
+export default class Home extends Component<Props, DefaultProps, State> {
+  constructor(props) {
+    super(props);
+    this.state = { roomName: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('A room name was submitted: ' + this.state.roomName);
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({ roomName: event.target.value });
+  }
+
   render() {
     return (
       <div className={styles.container}>
@@ -14,8 +34,8 @@ export default class Home extends Component {
         <h3>
           Enter chat room name you want to join or create
         </h3>
-        <form>
-          <input type="text" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.roomName} onChange={this.handleChange} />
           <button type="submit">
             Join
           </button>
